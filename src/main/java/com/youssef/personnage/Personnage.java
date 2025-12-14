47390e984d9b0bb09c3fcb0a5d032b258f825d29
Youssef Izoukha <239839971+Mr-usf@users.noreply.github.com>
2025-12-14 16:12:52 +0100

    cycle2: refactor rotation logic

--- src/main/java/com/youssef/personnage/Personnage.java
+++ src/main/java/com/youssef/personnage/Personnage.java
@@ -1,15 +1,19 @@
 package com.youssef.personnage;
 
 public class Personnage {
-    private static String ORIENTATIONS = "NORD";
+    private static final String[] ORIENTATIONS = {
+            "NORD", "EST", "SUD", "OUEST"
+    };
 
+    private int i = 0;
 
+    public String getOrientation() {
+        return ORIENTATIONS[i];
+    }
 
     public String tourner(int fois) {
-        if (fois == 1) {
-            ORIENTATIONS = "EST";
-        }
-        return ORIENTATIONS;
+        i = (i + fois) % 4;
+        return ORIENTATIONS[i];
     }
 
 }
